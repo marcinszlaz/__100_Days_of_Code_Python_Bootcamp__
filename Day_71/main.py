5d8ad0d528491cf15065a67305e61fdd5415aa90
Marcin Szlaz <marcinszlaz@gmail.com>
2026-05-27 17:51:13 +0200

    host="0.0.0.0"

--- Day_71/main.py
+++ Day_71/main.py
@@ -267,4 +267,4 @@ def contact():
 #         connection.sendmail(MAIL_ADDRESS, MAIL_APP_PW, email_message)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
