b37b6aa8429c78ed9c665d3edb3221422089d05a
Marcin Szlaz <marcinszlaz@gmail.com>
2026-05-27 17:55:07 +0200

    its boring

--- Day_71/main.py
+++ Day_71/main.py
@@ -267,4 +267,4 @@ def contact():
 #         connection.sendmail(MAIL_ADDRESS, MAIL_APP_PW, email_message)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, port=5001)
