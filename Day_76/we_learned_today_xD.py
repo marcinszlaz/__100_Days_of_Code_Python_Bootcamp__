164633557a43aab4d695432b801d4443b4070f60
Marcin Szlaz <marcinszlaz@gmail.com>
2026-06-16 17:50:22 +0200

    76 finished

---
+++ Day_76/we_learned_today_xD.py
@@ -0,0 +1,13 @@
+In this lesson we looked at how to:
+
+Pull a random sample from a DataFrame using .sample()
+
+How to find duplicate entries with .duplicated() and .drop_duplicates()
+
+How to convert string and object data types into numbers with .to_numeric()
+
+How to use plotly to generate beautiful pie, donut, and bar charts as well as box and scatter plots
+
+You can download the completed code for today in this lesson.
+
+Well done on completing today's lessons! 👏👏👏 I hope you enjoyed today’s Google Play Store analysis. It's incredible how much more of a clear picture we can get about the app ecosystem just by looking into some scraped website data for the Google Play Store.
